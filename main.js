4591308f4488d1c1795d812fc470c5c7285c5845
pbudzyns <pawel.budzynski@nokia.com>
2019-03-09 22:47:44 +0100

    snake consumes apple and grow

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 var blockSize = 40;
 var velocityX = 1;
 var velocityY = 0;
+const INITIAL_LENGHT = 5;
+var tail = INITIAL_LENGHT;
+var px; 
+var py;
 
 var appleX = 0;
 var appleY = 0;
@@ -38,13 +42,21 @@ window.onload = function() {
 function main(){
     moveSnake();
     if(snakeGetApple()){
-        getApple();
+        // extendSnake();
+        getNewApple();
+        tail++;
     }
     drawEverything();
 }
 
+function extendSnake(){
+    newX = appleX - velocityX * blockSize;
+    newY = appleY - velocityY * blockSize;
+    snake.push({x:newX, y:newY});
+}
+
 function snakeGetApple(){
-    if(snake[0].x == appleX && snake[0].y == appleY){
+    if(px == appleX && py == appleY){
         return true;
     } else {
         return false;
@@ -68,29 +80,37 @@ function drawApple(){
     colorRect(appleX, appleY, blockSize, blockSize, 'red');
 }
 
-function getApple(){
+function getNewApple(){
     appleX = Math.floor(Math.random() * canvas.width / blockSize) * blockSize;
     appleY = Math.floor(Math.random() * canvas.height / blockSize) * blockSize;
 }
 
 function moveSnake(){
-    for(var i=0;i<snake.length;i++){
-        if(snake[i].x >= canvas.width) {
-            snake[i].x = 0;
-        } else if (snake[i].x < 0) {
-            snake[i].x = canvas.width - blockSize;
-        } else {
-            snake[i].x += velocityX * blockSize;
-        }
+    px += velocityX * blockSize;
+    py += velocityY * blockSize;
+    if(px < 0) {
+        px = canvas.width - blockSize;
+    } else if (px > canvas.width - blockSize) {
+        px = 0;
+    } else if (py < 0 ) {
+        py = canvas.height - blockSize;
+    } else if (py > canvas.height - blockSize) {
+        py = 0;
+    }
 
-        if(snake[i].y >= canvas.height) {
-            snake[i].y = 0;
-        } else if (snake[i].y < 0) {
-            snake[i].y = canvas.height - blockSize;
-        } else {
-            snake[i].y += velocityY * blockSize;
+    for(var i=0;i<snake.length;i++){
+        colorRect(snake[i].x, snake[i].y, blockSize, blockSize, 'lime');
+        if(snake[i].x == px && snake[i].y == py){
+            tail = INITIAL_LENGHT;
         }
     }
+
+    snake.push({x: px, y:py});
+
+    while(snake.length > tail){
+        snake.shift();
+    }
+
 }
 
 function drawSnake(snake){
@@ -103,6 +123,8 @@ function initializeSnake(){
     var snakeBody = [];
     startX = Math.floor(Math.random() * canvas.width / blockSize) * blockSize;
     startY = Math.floor(Math.random() * canvas.height / blockSize) * blockSize;
+    px = startX;
+    py = startY;
     snakeBody.push({x:startX, y:startY});
     return snakeBody;
 }
